1ec706f344b8719be68dba73133a90eace691cd1
Tony Fruzza <tony240zt@gmail.com>
2026-02-21 19:07:29 -0800

    Add websidplay.wasm to vendor/sid-player for SID playback functionality

--- plugin.rb
+++ plugin.rb
@@ -15,4 +15,13 @@ register_asset "stylesheets/common/sid-player.scss"
 after_initialize do
   # Ensure .sid is in the list of authorized extensions if the plugin is enabled
   # Admins should also add "sid" to the authorized_extensions site setting
+
+  # Copy websidplayfp vendor files into public/ so they are served as static
+  # assets at /sid-player/<filename> without going through the Ember build.
+  src_dir = File.join(File.dirname(__FILE__), "vendor", "sid-player")
+  dst_dir = File.join(Rails.root, "public", "sid-player")
+  FileUtils.mkdir_p(dst_dir)
+  Dir.glob(File.join(src_dir, "*")).each do |f|
+    FileUtils.cp(f, dst_dir) unless File.directory?(f)
+  end
 end
